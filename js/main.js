e37d38ee71969c5378f7b9dc221302e002669c93
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-22 15:16:38 +0200

    add function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 const myImgEl = document.getElementById("lamp");
 const myButtonEl = document.getElementById("switch");
 const mySwitchEl = document.getElementById("switch-input");
+
+myButtonEl.addEventListener("click", () => {
+  mySwitchEl.innerHTML = "SPEGNI";
+  myButtonEl.classList.toggle("btn-danger");
+});
