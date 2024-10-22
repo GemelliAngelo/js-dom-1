f9aedcf8d2a9a0ca7d4d688ce83418d4cbf0e8c1
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-22 15:44:40 +0200

    add img src in js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-const myImgEl = document.getElementById("lamp");
+const myFigureEl = document.getElementById("lamps");
+const myImageEl = document.createElement("img");
 const myButtonEl = document.getElementById("switch");
 const mySwitchEl = document.getElementById("switch-input");
+
+myFigureEl.append(myImageEl);
+myImageEl.src = "./img/white_lamp.png";
+myImageEl.className = "img-fluid";
 
 myButtonEl.addEventListener("click", () => {
   myImgEl.scr = "./img/yellow_lamp.png";
