598a0a64ccf386587555f44c255c28e951e04655
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-22 17:18:24 +0200

    turn of lamp doesnt work

--- js/main.js
+++ js/main.js
@@ -10,15 +10,12 @@ myImageEl.alt = "white-lamp";
 
 myButtonEl.addEventListener("click", () => {
   myImageEl.src = "./img/yellow_lamp.png";
-
-  if ((myImageEl.src = "./img/yellow_lamp.png")) {
-    myImageEl.src = "./img/white_lamp.png";
-  }
+  mySwitchEl.innerHTML = "SPEGNI";
 
   myButtonEl.classList.toggle("btn-danger");
-  if (mySwitchEl.innerHTML.includes("ACCENDI")) {
-    mySwitchEl.innerHTML = "SPEGNI";
-  } else if (mySwitchEl.innerHTML.includes("SPEGNI")) {
+
+  if (myImageEl.src.includes("./img/yellow_lamp.png")) {
+    myImageEl.src = "./img/white_lamp.png";
     mySwitchEl.innerHTML = "ACCENDI";
   }
 });
