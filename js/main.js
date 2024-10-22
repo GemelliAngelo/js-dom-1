5bb427f1b85af805c85d53af8f023b61eb9c7a3f
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-22 16:57:08 +0200

    img src stuck

--- js/main.js
+++ js/main.js
@@ -5,17 +5,16 @@ const mySwitchEl = document.getElementById("switch-input");
 
 myFigureEl.append(myImageEl);
 myImageEl.src = "./img/white_lamp.png";
-myImageEl.className = "img-fluid";
+myImageEl.className = "img-fluid d-block";
 myImageEl.alt = "white-lamp";
 
 myButtonEl.addEventListener("click", () => {
+  myImageEl.src = "./img/yellow_lamp.png";
+
   myButtonEl.classList.toggle("btn-danger");
   if (mySwitchEl.innerHTML.includes("ACCENDI")) {
     mySwitchEl.innerHTML = "SPEGNI";
   } else if (mySwitchEl.innerHTML.includes("SPEGNI")) {
     mySwitchEl.innerHTML = "ACCENDI";
-  }
-
-  if (myImageEl.src.includes()) {
   }
 });
